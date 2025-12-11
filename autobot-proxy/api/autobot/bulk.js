4cc47c3dd88d8b77919df4be685ef2e141657ee6
ahmadbeee <91061600+ahmadbeee@users.noreply.github.com>
2025-12-11 10:37:54 +0100

    Add files via upload

---
+++ autobot-proxy/api/autobot/bulk.js
@@ -0,0 +1,32 @@
+export const config = {
+  runtime: 'edge'
+};
+
+export default async function handler(req) {
+  const centersUrl =
+    "http://autobot.multilentjmb.com:8080/autobotmonitor/cms/545975484454/api/centers/centers_with_systems";
+
+  try {
+    const res = await fetch(centersUrl);
+    const centers = await res.json();
+
+    const examRequests = centers.map(c =>
+      fetch(
+        `http://autobot.multilentjmb.com:8080/autobotmonitor/cms/545975484454/api/centers/center_last_exam?ref=${c.Ref}`
+      )
+        .then(r => r.json())
+        .catch(() => ({ error: "failed", ref: c.Ref }))
+    );
+
+    const examResults = await Promise.all(examRequests);
+
+    return new Response(JSON.stringify(examResults), {
+      headers: { "Content-Type": "application/json" }
+    });
+
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+}
---
+++ autobot-proxy/api/autobot/index.js
@@ -0,0 +1,44 @@
+export const config = {
+  runtime: 'edge'
+};
+
+export default async function handler(req) {
+  const { searchParams } = new URL(req.url);
+  const endpoint = searchParams.get("endpoint");
+  const ref = searchParams.get("ref");
+
+  let apiUrl;
+
+  if (endpoint === "centers") {
+    apiUrl =
+      "http://autobot.multilentjmb.com:8080/autobotmonitor/cms/545975484454/api/centers/centers_with_systems";
+
+  } else if (endpoint === "center_last_exam") {
+    if (!ref) {
+      return new Response(JSON.stringify({ error: "Missing ref parameter" }), {
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    apiUrl =
+      `http://autobot.multilentjmb.com:8080/autobotmonitor/cms/545975484454/api/centers/center_last_exam?ref=${ref}`;
+
+  } else {
+    return new Response(JSON.stringify({ error: "Invalid endpoint" }), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.text();
+    return new Response(data, {
+      headers: { "Content-Type": "application/json" }
+    });
+
+  } catch (err) {
+    return new Response(JSON.stringify({ error: err.message }), {
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+}
